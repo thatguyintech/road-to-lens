e1886af1708385e5df04ce8ee3ded8d9892d5913
ALBERT <thatguyintech@gmail.com>
2022-07-14 03:40:48 -0700

    [Step 1] Try Apollo on index.js with Recommended Profiles from Lens

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo-client";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp
